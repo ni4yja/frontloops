38e0bf09e3607c28529f29d762f4281a531a34e2
ni4yja <k2.liuba@gmail.com>
2020-07-20 21:37:05 +0300

    task 11 complete

--- task-11/script.js
+++ task-11/script.js
@@ -1,18 +1,16 @@
 var keys = ['Synthetic', 'Wool', 'Denim', 'Silk', 'Linen']    
 
 var mySwiper = new Swiper('.swiper-container', {
-    //effect: 'fade',
-    speed: 3000,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-    },
     pagination: {
-        el: '.elements',
+        el: '.swiper-pagination',
         clickable: true,
         renderBullet: function (index, className) {
             return '<div class="' + className + '">' + (keys[index]) + 
             '</div>';
         },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     }
 });
